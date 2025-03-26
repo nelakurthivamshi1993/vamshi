dc73ff2f9521440a508fa3633649f07c3462f76d
nelakurthivamshi1993 <nelakurthivamshireddy@gmail.com>
2025-03-26 07:02:49 +0530

    Added explorer

--- citation-ui/src/app/app.component.ts
+++ citation-ui/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CitationListComponent } from './citation-list/citation-list.component';
-import { RouterOutlet, RouterLink } from '@angular/router'; // Import RouterOutlet and RouterLink
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CitationListComponent, RouterOutlet, RouterLink], // Import RouterOutlet and RouterLink
+  imports: [CitationListComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
